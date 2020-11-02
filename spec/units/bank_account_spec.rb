5197a4534503f75a3480dd9eeabc86917cfeff67
Veronica Lee <veronicavlee@icloud.com>
2020-11-02 20:52:45 +0000

    Add unit test for making deposit on bank account

--- spec/units/bank_account_spec.rb
+++ spec/units/bank_account_spec.rb
@@ -1,17 +1,33 @@
 require 'bank_account'
 
 describe BankAccount do
+  describe '#deposit' do
+    it 'creates a credit transaction and stores it in the account transactions' do
+      transaction_dbl = double('transaction', type: :credit)
+      transaction_class_dbl = double('transaction_class', new: transaction_dbl)
+      time_dbl = Time.parse("2020-11-02 12:25:15")
+      allow(Time).to receive(:now).and_return(time_dbl)
+      account = BankAccount.new(transaction_class: transaction_class_dbl)
+
+      expect(transaction_class_dbl).to receive(:new).once.with({amount: 50, datetime: time_dbl, type: :credit})
+      expect{ account.deposit(50) }.to change{ account.transactions.count }.by(1)
+    end
+  end
+
   describe '#print_statement' do
-    it 'creates a statement with the given transactions and prints statement' do
-      account = BankAccount.new
+    it "creates a statement with the account's transactions and instructs statement to print" do
+      transaction_dbl = double('transaction')
+      transaction_class_dbl = double('transaction_class', new: transaction_dbl)
+      account = BankAccount.new(transaction_class: transaction_class_dbl)
       statement_dbl = double('statement')
       statement_class_dbl = double('statement_class', new: statement_dbl)
-      transactions_dbl = ['transaction double']
 
-      expect(statement_class_dbl).to receive(:new).once.with(transactions_dbl)
+      account.deposit(50)
+
+      expect(statement_class_dbl).to receive(:new).once.with([transaction_dbl])
       expect(statement_dbl).to receive(:print).once
 
-      account.print_statement(transactions: transactions_dbl, statement_class: statement_class_dbl)
+      account.print_statement(statement_class: statement_class_dbl)
     end
   end
 end
