63fd3bbb2fcb2b32c004c3ebb10041c24deee2d2
Veronica Lee <veronicavlee@icloud.com>
2020-11-02 13:11:30 +0000

    Update print_statement to handle a single transaction of varying amounts:
    - Formats the amount to 2 decimal places for printing

--- lib/bank.rb
+++ lib/bank.rb
@@ -2,14 +2,19 @@ require 'time'
 
 class Bank
   def initialize
-
+    @transactions = []
   end
 
   def deposit(amount)
-
+    @transactions << { type: :credit, amount: amount }
   end
 
   def print_statement
-    print("date || credit || debit || balance\n02/11/2020 || 50.00 || || 50.00")
+    puts("date || credit || debit || balance")
+    puts("02/11/2020 || #{format_amount(@transactions[0][:amount])} || || #{format_amount(@transactions[0][:amount])}")
+  end
+
+  def format_amount(amount)
+    '%.2f' % amount
   end
 end
--- spec/units/bank_spec.rb
+++ spec/units/bank_spec.rb
@@ -5,7 +5,14 @@ describe Bank do
     bank = Bank.new
     bank.deposit(50.00)
     allow(Time).to receive(:now).and_return(Time.parse("2020-11-02 12:25:15"))
-    expect{ bank.print_statement }.to output("date || credit || debit || balance\n02/11/2020 || 50.00 || || 50.00").to_stdout
+    expect{ bank.print_statement }.to output("date || credit || debit || balance\n02/11/2020 || 50.00 || || 50.00\n").to_stdout
+  end
+
+  it 'prints a statement containing 02/11/2020 || 500.00 || || 500.00 when a deposit of 500.00 has been made' do
+    bank = Bank.new
+    bank.deposit(500.00)
+    allow(Time).to receive(:now).and_return(Time.parse("2020-11-02 12:25:15"))
+    expect{ bank.print_statement }.to output("date || credit || debit || balance\n02/11/2020 || 500.00 || || 500.00\n").to_stdout
   end
 
 end
