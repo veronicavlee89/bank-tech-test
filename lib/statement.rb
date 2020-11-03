a615fd21eb77c4d39fa5d446ecf2da277bd45800
Veronica Lee <veronicavlee@icloud.com>
2020-11-03 10:46:16 +0000

    Refactor Statement to improve code maintainability:
    - Split out methods to follow SRP
    
    - Store statement rows in array, and join at time of printing. This is more future proof/easier to manipulate than storing a single long string - e.g. for things like a max. rows printed per page.

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,22 +1,33 @@
 class Statement
 
   def initialize(transactions)
-    @text = ""
-    @text += "date || credit || debit || balance\n"
-    running_balance = 0
-    formatted_transactions = []
-    transactions.each do |transaction|
-      running_balance += transaction.amount
-      formatted_transactions << "#{format_date(transaction.datetime)} || #{format_amount(transaction.amount)} || || #{format_amount(running_balance)}\n"
-    end
-    @text += formatted_transactions.reverse.join
+    @rows = []
+    add_header
+    add_transactions(transactions)
   end
 
   def print
-    puts @text
+    puts @rows.join("\n")
   end
 
   private
+
+  def add_header
+    @rows << "date || credit || debit || balance"
+  end
+
+  def add_transactions(transactions)
+    running_balance = 0
+    formatted_transactions = transactions.map do |transaction|
+      running_balance += transaction.amount
+      format_transaction(transaction, running_balance)
+    end
+    @rows << formatted_transactions.reverse
+  end
+
+  def format_transaction(transaction, running_balance)
+    "#{format_date(transaction.datetime)} || #{format_amount(transaction.amount)} || || #{format_amount(running_balance)}"
+  end
 
   def format_amount(amount)
     '%.2f' % amount
