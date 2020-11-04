aa8f23dce64007ef6c2a2e6430c39ff5c13f951e
Veronica Lee <veronicavlee@icloud.com>
2020-11-04 13:38:57 +0000

    Rename dbl to double in unit tests

--- spec/units/bank_account_spec.rb
+++ spec/units/bank_account_spec.rb
@@ -2,31 +2,29 @@ require 'bank_account'
 
 describe BankAccount do
 
-  subject(:account) { BankAccount.new }
-
   describe '#deposit' do
     context 'date not specified by user' do
       it 'creates a credit transaction and stores it in the account transactions with the current datetime' do
-        transaction_dbl = double('transaction', type: :credit)
-        transaction_class_dbl = double('transaction_class', new: transaction_dbl)
-        time_dbl = Time.parse("2020-11-02 12:25:15")
-        allow(Time).to receive(:now).and_return(time_dbl)
-        account = BankAccount.new(transaction_class: transaction_class_dbl)
+        transaction_double = double('transaction', type: :credit)
+        transaction_class_double = double('transaction_class', new: transaction_double)
+        time_double = Time.parse("2020-11-02 12:25:15")
+        allow(Time).to receive(:now).and_return(time_double)
+        account = BankAccount.new(transaction_class: transaction_class_double)
 
-        expect(transaction_class_dbl).to receive(:new).once.with({amount: 50, datetime: time_dbl, type: :credit})
+        expect(transaction_class_double).to receive(:new).once.with({amount: 50, datetime: time_double, type: :credit})
         expect{ account.deposit(50) }.to change{ account.transactions.count }.by(1)
       end
     end
 
     context 'date specified by user' do
       it 'creates a credit transaction with the given date and stores it in the account transactions' do
-        transaction_dbl = double('transaction', type: :credit)
-        transaction_class_dbl = double('transaction_class', new: transaction_dbl)
-        account = BankAccount.new(transaction_class: transaction_class_dbl)
+        transaction_double = double('transaction', type: :credit)
+        transaction_class_double = double('transaction_class', new: transaction_double)
+        account = BankAccount.new(transaction_class: transaction_class_double)
 
         expected_date = Time.parse('01/01/2020')
 
-        expect(transaction_class_dbl).to receive(:new).once.with({amount: 50, datetime: expected_date, type: :credit})
+        expect(transaction_class_double).to receive(:new).once.with({amount: 50, datetime: expected_date, type: :credit})
         expect{ account.deposit(50, '01/01/2020') }.to change{ account.transactions.count }.by(1)
       end
     end
@@ -35,26 +33,26 @@ describe BankAccount do
   describe '#withdraw' do
     context 'date not specified by user' do
       it 'creates a debit transaction and stores it in the account transactions with the current datetime' do
-        transaction_dbl = double('transaction', type: :debit)
-        transaction_class_dbl = double('transaction_class', new: transaction_dbl)
-        time_dbl = Time.parse("2020-11-03 16:01:00")
-        allow(Time).to receive(:now).and_return(time_dbl)
-        account = BankAccount.new(transaction_class: transaction_class_dbl)
+        transaction_double = double('transaction', type: :debit)
+        transaction_class_double = double('transaction_class', new: transaction_double)
+        time_double = Time.parse("2020-11-03 16:01:00")
+        allow(Time).to receive(:now).and_return(time_double)
+        account = BankAccount.new(transaction_class: transaction_class_double)
 
-        expect(transaction_class_dbl).to receive(:new).once.with({amount: 50, datetime: time_dbl, type: :debit})
+        expect(transaction_class_double).to receive(:new).once.with({amount: 50, datetime: time_double, type: :debit})
         expect{ account.withdraw(50) }.to change{ account.transactions.count }.by(1)
       end
     end
 
     context 'date specified by user' do
       it 'creates a debit transaction with the given date and stores it in the account transactions' do
-        transaction_dbl = double('transaction', type: :credit)
-        transaction_class_dbl = double('transaction_class', new: transaction_dbl)
-        account = BankAccount.new(transaction_class: transaction_class_dbl)
+        transaction_double = double('transaction', type: :credit)
+        transaction_class_double = double('transaction_class', new: transaction_double)
+        account = BankAccount.new(transaction_class: transaction_class_double)
 
         expected_date = Time.parse('01/01/2020')
 
-        expect(transaction_class_dbl).to receive(:new).once.with({amount: 50, datetime: expected_date, type: :debit})
+        expect(transaction_class_double).to receive(:new).once.with({amount: 50, datetime: expected_date, type: :debit})
         expect{ account.withdraw(50, '01/01/2020') }.to change{ account.transactions.count }.by(1)
       end
     end
@@ -62,19 +60,19 @@ describe BankAccount do
 
   describe '#print_statement' do
     it "creates a statement with the account's transactions and instructs statement to print" do
-      transaction_dbl = double('transaction')
-      transaction_class_dbl = double('transaction_class', new: transaction_dbl)
-      account = BankAccount.new(transaction_class: transaction_class_dbl)
-      statement_dbl = double('statement')
-      statement_class_dbl = double('statement_class', new: statement_dbl)
+      transaction_double = double('transaction')
+      transaction_class_double = double('transaction_class', new: transaction_double)
+      account = BankAccount.new(transaction_class: transaction_class_double)
+      statement_double = double('statement')
+      statement_class_double = double('statement_class', new: statement_double)
 
       account.deposit(50)
       account.withdraw(30)
 
-      expect(statement_class_dbl).to receive(:new).once.with([transaction_dbl, transaction_dbl])
-      expect(statement_dbl).to receive(:print).once
+      expect(statement_class_double).to receive(:new).once.with([transaction_double, transaction_double])
+      expect(statement_double).to receive(:print).once
 
-      account.print_statement(statement_class: statement_class_dbl)
+      account.print_statement(statement_class: statement_class_double)
     end
   end
 end
--- spec/units/statement_spec.rb
+++ spec/units/statement_spec.rb
@@ -13,34 +13,34 @@ end
 
 describe Statement do
   it 'prints a statement containing 02/11/2020 || 50.25 || || 50.25 given a credit transaction of 50.25' do
-    transaction_dbl = TransactionDouble.new(type: :credit, amount: 50.25, datetime: Time.parse("2020-11-02 10:50:15"))
-    statement = Statement.new([transaction_dbl])
+    transaction_double = TransactionDouble.new(type: :credit, amount: 50.25, datetime: Time.parse("2020-11-02 10:50:15"))
+    statement = Statement.new([transaction_double])
     expect{ statement.print }.to output("date || credit || debit || balance\n02/11/2020 || 50.25 || || 50.25\n").to_stdout
   end
 
   it 'prints a statement containing 02/11/2020 || || 50.25 || -50.25 given a debit transaction of 50.25' do
-    transaction_dbl = TransactionDouble.new(type: :debit, amount: 50.25, datetime: Time.parse("2020-11-02 10:50:15"))
-    statement = Statement.new([transaction_dbl])
+    transaction_double = TransactionDouble.new(type: :debit, amount: 50.25, datetime: Time.parse("2020-11-02 10:50:15"))
+    statement = Statement.new([transaction_double])
     expect{ statement.print }.to output("date || credit || debit || balance\n02/11/2020 || || 50.25 || -50.25\n").to_stdout
   end
 
   it 'formats amounts with no decimal places to have 2 decimals' do
-    transaction_dbl = TransactionDouble.new(type: :credit, amount: 20, datetime: Time.parse("2020-11-02 10:50:15"))
-    statement = Statement.new([transaction_dbl])
+    transaction_double = TransactionDouble.new(type: :credit, amount: 20, datetime: Time.parse("2020-11-02 10:50:15"))
+    statement = Statement.new([transaction_double])
     expect{ statement.print }.to output("date || credit || debit || balance\n02/11/2020 || 20.00 || || 20.00\n").to_stdout
   end
 
   it 'formats amounts with 1 decimal place to have 2 decimals' do
-    transaction_dbl = TransactionDouble.new(type: :credit, amount: 12.5, datetime: Time.parse("2020-11-02 10:50:15"))
-    statement = Statement.new([transaction_dbl])
+    transaction_double = TransactionDouble.new(type: :credit, amount: 12.5, datetime: Time.parse("2020-11-02 10:50:15"))
+    statement = Statement.new([transaction_double])
     expect{ statement.print }.to output("date || credit || debit || balance\n02/11/2020 || 12.50 || || 12.50\n").to_stdout
   end
 
   it 'formats one transaction per line with a running balance, in reverse chronological order of datetime' do
-    transaction_dbl_1 = TransactionDouble.new(type: :credit, amount: 20, datetime: Time.parse("2020-10-30 15:22:08"))
-    transaction_dbl_2 = TransactionDouble.new(type: :debit, amount: 12.5, datetime: Time.parse("2020-11-05 10:50:15"))
-    transaction_dbl_3 = TransactionDouble.new(type: :credit, amount: 120.5, datetime: Time.parse("2020-11-03 08:00:15"))
-    transactions = [transaction_dbl_1, transaction_dbl_2, transaction_dbl_3]
+    transaction_double_1 = TransactionDouble.new(type: :credit, amount: 20, datetime: Time.parse("2020-10-30 15:22:08"))
+    transaction_double_2 = TransactionDouble.new(type: :debit, amount: 12.5, datetime: Time.parse("2020-11-05 10:50:15"))
+    transaction_double_3 = TransactionDouble.new(type: :credit, amount: 120.5, datetime: Time.parse("2020-11-03 08:00:15"))
+    transactions = [transaction_double_1, transaction_double_2, transaction_double_3]
     statement = Statement.new(transactions)
 
     expect{ statement.print }.to output(
